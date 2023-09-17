043d3c8c98e6689d8f782c5f4098fd174df93c9b
gus <gustavo_9_rj@hotmail.com>
2023-09-18 00:55:19 +0200

    feat: added some components

---
+++ src/component/BaseHtml.tsx
@@ -0,0 +1,20 @@
+import * as elements from "typed-html";
+
+const BaseHtml = ({ children }: elements.Children) => `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <script src="https://unpkg.com/htmx.org@1.9.5" integrity="sha384-xcuj3WpfgjlKF+FXhSQFQ0ZNr39ln+hwjN3npfM9VBnUskLolQAcN80McRIVOPuO" crossorigin="anonymous"></script>
+  <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio"></script>
+  <title>inHouse</title>
+</head>
+<body>
+  ${children}
+</body>
+</html>
+`;
+
+export default BaseHtml;
--- src/index.tsx
+++ src/index.tsx
@@ -1,42 +1,23 @@
 import html from "@elysiajs/html";
 import { Elysia } from "elysia";
 import * as elements from "typed-html";
+import Home from "./application/services/home/Home";
+import SignIn from "./application/services/auth/signin/SignIn";
+import Dashboard from "./application/services/dashboard/Dashboard";
 
 const app = new Elysia()
   .use(html())
-  .get("/", ({ html }) =>
-    html(
-      <BaseHtml>
-        <body class="flex w-full h-screen justify-center items-center" hx-post="/click-me" hx-swap="outerHTML">
-          <button>Click me!</button>
-        </body>
-      </BaseHtml>
-    )
-  )
+  .get("/clicked", () => <div>Clicked!</div>)
+  .get("/", () => <Home />)
+  .get("/sign-in", () => <SignIn />)
+  .get("/dashboard", () => <Dashboard />)
   .post("/click-me", () => <div class="hover:text-blue-500">Clicked!</div>)
   .listen(3000);
 
 console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
 
-const host = Bun.env.SUPABASE_URL;
-const password = Bun.env.SUPABASE_PASSWORD;
-const anonKey = Bun.env.SUPABASE_ANON_KEY;
+// const host = Bun.env.SUPABASE_URL;
+// const password = Bun.env.SUPABASE_PASSWORD;
+// const anonKey = Bun.env.SUPABASE_ANON_KEY;
 
-console.log(host, password, anonKey);
-
-const BaseHtml = ({ children }: elements.Children) => `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <script src="https://unpkg.com/htmx.org@1.9.5"></script>
-  <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
-  <title>The BETH stack</title>
-</head>
-<body>
-  ${children}
-</body>
-</html>
-`;
+// console.log(host, password, anonKey);
