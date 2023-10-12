ba775e9a414aa97dfe0b049b488076006b77e0c3
Chemchu <gustavo_9_rj@hotmail.com>
2023-10-12 17:44:22 +0200

    feat: barebones added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello mom!");
+mod pages;
+
+use axum::{
+    http::StatusCode,
+    response::{Html, IntoResponse},
+    routing::get,
+    Router,
+};
+use pages::index::IndexTemplate;
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(handle_main));
+
+    let addr_str = "127.0.0.1:3000";
+    let addr = addr_str.parse::<SocketAddr>().unwrap();
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn handle_main() -> impl IntoResponse {
+    let template = IndexTemplate {};
+    let reply_html = askama::Template::render(&template).unwrap();
+    (StatusCode::OK, Html(reply_html).into_response())
 }
