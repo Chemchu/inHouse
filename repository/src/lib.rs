9cdd70f255f1e8bf7c126bf48423f0729b0175aa
Chemchu <gustavo_9_rj@hotmail.com>
2023-11-20 01:27:56 +0100

    refactor: removed commented code

--- repository/src/lib.rs
+++ repository/src/lib.rs
@@ -1,7 +1,7 @@
 use std::time::Duration;
 
 use migration::{Migrator, MigratorTrait};
-use sea_orm::{ConnectOptions, ConnectionTrait, Database, DatabaseConnection};
+use sea_orm::{ConnectOptions, Database, DatabaseConnection};
 
 pub mod auth;
 
@@ -31,31 +31,5 @@ pub async fn connect_to_db() -> Result<DatabaseConnection, sea_orm::DbErr> {
     let db = Database::connect(opt).await?;
     Migrator::up(&db, None).await?;
 
-    // match initialize_schema(&db).await {
-    //     Ok(_) => {}
-    //     Err(err) => {
-    //         tracing::error!("Error initializing database schema: {:?}", err);
-    //         std::process::exit(1);
-    //     }
-    // }
-
     Ok(db)
-}
-
-async fn initialize_schema(db: &DatabaseConnection) -> Result<(), sea_orm::DbErr> {
-    tracing::info!("Initializing database schema...");
-
-    let backend = db.get_database_backend();
-    let schema = sea_orm::Schema::new(backend);
-    let table_create_statement = schema.create_table_from_entity(entity::user_metadata::Entity);
-    match db.execute(backend.build(&table_create_statement)).await {
-        Ok(_) => {
-            tracing::info!("Database schema initialized!");
-            Ok(())
-        }
-        Err(err) => {
-            tracing::error!("Error creating table: {:?}", err);
-            std::process::exit(1);
-        }
-    }
 }
