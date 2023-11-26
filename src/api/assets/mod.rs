7a364e0c125cbfb9088686967f505475143c461f
Chemchu <gustavo_9_rj@hotmail.com>
2023-11-26 17:21:43 +0100

    feat: fixed spinner by adding local svg

--- src/api/assets/mod.rs
+++ src/api/assets/mod.rs
@@ -10,10 +10,10 @@ use tower_http::services::ServeDir;
 pub fn routes() -> Router {
     Router::new()
         .route("/_assets/*path", get(assets_handler))
-        .nest_service("/_assets/icons", ServeDir::new("assets/icons"))
+        .nest_service("/_assets/icons", ServeDir::new("src/assets/icons"))
         .nest_service(
             "/_assets/pwa/screenshots",
-            ServeDir::new("assets/pwa/screenshots"),
+            ServeDir::new("src/assets/pwa/screenshots"),
         )
 }
 
@@ -21,7 +21,6 @@ static STYLE_CSS: &str = include_str!("../../assets/style.css");
 static FAVICON: &str = include_str!("../../assets/favicon.svg");
 static HTMX: &str = include_str!("../../assets/htmx.min.js");
 static HYPERSCRIPT: &str = include_str!("../../assets/hyperscript.min.js");
-static LOADER: &str = include_str!("../../assets/loader.svg");
 static MANIFEST: &str = include_str!("../../assets/pwa/manifest.json");
 static NONE: &str = "";
 
@@ -45,10 +44,6 @@ async fn assets_handler(Path(path): Path<String>) -> impl IntoResponse {
         "hyperscript.min.js" => {
             headers.insert(header::CONTENT_TYPE, "text/javascript".parse().unwrap());
             (StatusCode::OK, headers, HYPERSCRIPT)
-        }
-        "loader.svg" => {
-            headers.insert(header::CONTENT_TYPE, "image/svg+xml".parse().unwrap());
-            (StatusCode::OK, headers, LOADER)
         }
         "manifest.json" => {
             headers.insert(header::CONTENT_TYPE, "application/json".parse().unwrap());
