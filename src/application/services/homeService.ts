db2e9f865f7980ebcab3f74d73957fe98caac4ec
Chemchu <gustavo_9_rj@hotmail.com>
2023-12-02 15:45:27 +0100

    fix: removed remaining TS files

--- src/application/services/homeService.ts
+++
@@ -1,9 +0,0 @@
-import Elysia from "elysia";
-import Home from "../../pages/Home";
-
-const homeService = () => {
-    return new Elysia()
-        .get("/", Home)
-}
-
-export default homeService;
--- src/middleware/mainHtml.ts
+++
@@ -1,8 +0,0 @@
-import Elysia from "elysia";
-
-const mainHtml = () => {
-    return new Elysia()
-
-}
-
-export default mainHtml;
