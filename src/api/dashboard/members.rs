50614e5788b17c6527e2630df05b013786251013
Chemchu <gustavo_9_rj@hotmail.com>
2023-12-03 13:34:00 +0100

    feat: added skeleton dashboard pages

---
+++ src/api/dashboard/members.rs
@@ -0,0 +1,28 @@
+use askama::Template;
+use axum::{
+    extract::State,
+    response::{Html, IntoResponse},
+};
+use reqwest::StatusCode;
+use service::Token;
+
+#[derive(Template)]
+#[template(path = "dashboard/members/index.html")]
+struct MembersTemplate {
+    translator: i18n::Translator,
+    email: String,
+}
+
+pub async fn members_page_handler(
+    State(state): State<service::AppState>,
+    Token(claims): Token,
+) -> impl IntoResponse {
+    let template = MembersTemplate {
+        translator: state.translator.clone(),
+        email: claims.email,
+    };
+
+    let reply_html = askama::Template::render(&template).unwrap();
+
+    (StatusCode::OK, Html(reply_html).into_response())
+}
--- src/api/dashboard/mod.rs
+++ src/api/dashboard/mod.rs
@@ -1,18 +1,30 @@
 use axum::{middleware, routing::get, Router};
 
+use self::chats::chats_page_handler;
 use self::groceries::groceries_page_handler;
 use self::home::dashboard_home_page_handler;
+use self::members::members_page_handler;
+use self::payments::payments_page_handler;
+use self::summary::summary_page_handler;
 
 use super::layer;
 
+pub mod chats;
 pub mod groceries;
 pub mod home;
+pub mod members;
+pub mod payments;
+pub mod summary;
 pub mod user_settings;
 
 pub fn routes(state: service::AppState) -> Router {
     Router::new()
         .route("/dashboard", get(dashboard_home_page_handler))
+        .route("/dashboard/members", get(members_page_handler))
         .route("/dashboard/groceries", get(groceries_page_handler))
+        .route("/dashboard/chats", get(chats_page_handler))
+        .route("/dashboard/payments", get(payments_page_handler))
+        .route("/dashboard/summary", get(summary_page_handler))
         .route(
             "/dashboard/settings",
             get(user_settings::settings_page_handler),
