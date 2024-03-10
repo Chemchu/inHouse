2eefd894793241fb91aa3f31edb2de23306ccb6d
Chemchu <gustavo_9_rj@hotmail.com>
2024-03-11 00:12:38 +0100

    feat: adding migrations changes'

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,12 +1,48 @@
-struct Migrator;
+use surrealdb::{
+    engine::remote::ws::{Client, Ws},
+    Surreal,
+};
+
+struct Migrator {
+    db: Surreal<Client>,
+    migrations: Vec<&'static str>,
+}
+
+impl Migrator {
+    pub async fn new() -> Self {
+        Migrator {
+            db: Surreal::new::<Ws>("127.0.0.1:8000")
+                .await
+                .expect("Failed to connect to the server"),
+            migrations: Vec::new(),
+        }
+    }
+
+    pub fn add_migration(&mut self, migration: &'static str) -> &mut Self {
+        self.migrations.push(migration);
+        self
+    }
+}
 
 trait MigratorTrait {
-    async fn migrations() -> surrealdb::Result<()>;
+    fn migrations(&mut self);
+    async fn up(&self) -> surrealdb::Result<()>;
+    async fn down(&self) -> surrealdb::Result<()>;
 }
 
 impl MigratorTrait for Migrator {
-    async fn migrations() -> surrealdb::Result<()> {
-        // Add migrations here
+    fn migrations(&mut self) {
+        self.add_migration(include_str!("../database.surql"))
+            .add_migration(include_str!("../database.surql"));
+    }
+
+    // TODO: Probar a ver como ejecutar todas las migraciones usando async/await o tokio para que
+    // funcione db.query(...)
+    async fn up(&self) -> Result<(), surrealdb::Error> {
+        todo!()
+    }
+
+    async fn down(&self) -> surrealdb::Result<()> {
         todo!()
     }
 }
